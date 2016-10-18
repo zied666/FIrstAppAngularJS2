3e798cda854894a26d70024981b3e9a6ee760aac
Zied Kharraz <zied.kharraz@gmail.com>
2016-10-18 10:15:19 +0100

    make filter hotel dynamique

--- app/hotel/hotel.list.component.ts
+++ app/hotel/hotel.list.component.ts
@@ -1,4 +1,3 @@
-// Exact copy except import UserService from core
 import {Component, OnInit}   from '@angular/core';
 import {HotelService} from "./hotel.service";
 import {Hotel} from "./hotel";
@@ -7,42 +6,43 @@ import {Hotel} from "./hotel";
     moduleId: module.id,
     templateUrl: 'hotel.list.html'
 })
+
 export class HotelListComponent implements OnInit {
 
     hotels: Hotel[];
-    search: Object={
-        checkIn: new Date(),
-        limit: 10,
-        offset: 0,
-        nuitees: 1,
-        nom: null,
-        order: "ASC",
-        orderBy: "libelle",
-        ville: null,
-        etoiles: null,
-    };
+    search: Object;
     loadingList: Boolean;
 
     constructor(private hotelService: HotelService) {
     }
 
+
+
     ngOnInit() {
-        /*this.search = {
-            checkIn: new Date(),
+        this.search = {
+            checkIn: new Date().toISOString().slice(0,10).replace(/-/g,"-"),
             limit: 10,
             offset: 0,
             nuitees: 1,
-            nom: null,
+            nom: "",
             order: "ASC",
             orderBy: "libelle",
-            ville: null,
-            etoiles: null,
-        };*/
+            ville: "",
+            etoiles: "",
+        };
         this.loadingList = true;
-        this.hotelService.getHotels().then(hotels => {
+        this.hotelService.getHotels(this.search).then(hotels => {
             this.loadingList = false;
             this.hotels = hotels;
         });
         ;
+    }
+    onChange() {
+        this.loadingList = true;
+        this.hotels=[];
+        this.hotelService.getHotels(this.search).then(hotels => {
+            this.loadingList = false;
+            this.hotels = hotels;
+        });
     }
 }
--- app/hotel/hotel.service.ts
+++ app/hotel/hotel.service.ts
@@ -1,25 +1,35 @@
 import {Injectable}    from '@angular/core';
-import {Headers, Http} from '@angular/http';
+import {Headers, Http, URLSearchParams} from '@angular/http';
 import 'rxjs/add/operator/toPromise';
 import {Hotel} from './hotel';
 
 
 @Injectable()
 export class HotelService {
-  private handleError(error: any): Promise<any> {
-    console.error('An error occurred', error); // for demo purposes only
-    return Promise.reject(error.message || error);
-  }
+    private handleError(error: any): Promise<any> {
+        console.error('An error occurred', error); // for demo purposes only
+        return Promise.reject(error.message || error);
+    }
 
-  private heroesUrl = 'http://os-travel.com/api/hotels?nuitees=1&checkIn=2016-05-05&limit=30&offset=5';  // URL to web api
-  constructor(private http: Http) {
-  }
+    private heroesUrl = 'http://os-travel.com/api/hotels';  // URL to web api
+    constructor(private http: Http) {
+    }
 
-  getHotels(): Promise<Hotel[]> {
-    return this.http.get(this.heroesUrl)
-        .toPromise()
-        .then(response => response.json() as Hotel[])
-        .catch(this.handleError);
-  }
+    getHotels(search): Promise<Hotel[]> {
+        let params = new URLSearchParams();
+        params.set('nuitees', search.nuitees); // the user's search value
+        params.set('checkIn', search.checkIn);
+        params.set('limit', search.limit);
+        params.set('offset', search.offset);
+        params.set('search', search.nom);
+        params.set('ville', search.ville);
+        params.set('etoile', search.etoiles);
+        params.set('orderBy', search.orderBy);
+        params.set('order', search.order);
+        return this.http.get(this.heroesUrl, {search: params})
+            .toPromise()
+            .then(response => response.json() as Hotel[])
+            .catch(this.handleError);
+    }
 
 }
