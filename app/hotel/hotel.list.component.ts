0958b7985d04cbec00ccf292f3bbf772a62b39cd
Zied Kharraz <zied.kharraz@gmail.com>
2016-10-19 09:36:53 +0100

    make croll infinty dynamic

--- app/hotel/hotel.list.component.ts
+++ app/hotel/hotel.list.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit}   from '@angular/core';
 import {HotelService} from "./hotel.service";
 import {Hotel} from "./hotel";
 import {Search} from "./search";
+import {and} from "@angular/router/src/utils/collection";
 
 @Component({
     moduleId: module.id,
@@ -11,14 +12,17 @@ import {Search} from "./search";
 export class HotelListComponent implements OnInit {
 
     hotels: Hotel[];
-    search = new  Search();
+    search = new Search();
     loadingList: Boolean;
+    loadingMore: Boolean;
+    haveMore: Boolean;
 
     constructor(private hotelService: HotelService) {
     }
 
 
     ngOnInit() {
+        this.haveMore = true;
         this.search.checkIn = new Date().toISOString().slice(0, 10).replace(/-/g, "-");
         this.search.limit = 10;
         this.search.offset = 0;
@@ -38,16 +42,30 @@ export class HotelListComponent implements OnInit {
 
     onChange() {
         this.loadingList = true;
+        this.haveMore = true;
         this.hotels = [];
         this.search.offset = 0;
+        this.getHotels();
+    }
+
+    getHotels() {
         this.hotelService.getHotels(this.search).then(hotels => {
             this.loadingList = false;
-            this.hotels = hotels;
+            this.loadingMore = false;
+            if (hotels.length > 0)
+                this.hotels = this.hotels.concat(hotels);
+            else
+                this.haveMore = false;
         });
     }
 
     onScroll() {
-        console.log('scrolled!!')
+        if (!this.loadingList && !this.loadingMore && this.haveMore) {
+            this.search.offset += this.search.limit;
+            this.loadingMore = true;
+            console.log(this.search.offset);
+            this.getHotels();
+        }
     }
 
     updateSort(lib: string) {
