e1adb9216e24b73202412fb415ff38b8bc53f172
Zied Kharraz <zied.kharraz@gmail.com>
2016-10-21 12:01:35 +0100

    modif

--- app/shared/highlight.directive.ts
+++ app/shared/highlight.directive.ts
@@ -2,7 +2,8 @@
 // Exact copy of contact/highlight.directive except for color and message
 import {Directive, ElementRef, Renderer} from '@angular/core';
 
-@Directive({selector: '[highlight], input'})
+//@Directive({selector: '[highlight], input'})
+@Directive({selector: '[highlight]'})
 
 export class HighlightDirective {
     constructor(renderer: Renderer, el: ElementRef) {
