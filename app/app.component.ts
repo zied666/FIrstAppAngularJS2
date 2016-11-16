a7f2b240df1de46d7f02067ba8068150cdc6fac9
Zied Kharraz <zied.kharraz@gmail.com>
2016-11-16 10:40:20 +0100

    add loader

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnDestroy, OnInit} from '@angular/core';
+declare var $: any
 
 @Component({
   selector: 'my-app',
   template: '<router-outlet></router-outlet>'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private el: ElementRef) {
+  }
+
+  public ngOnInit() {
+    //$(this.el.nativeElement).dropdown();
+    $('body').addClass('loaded');
+    $('h1').css('color','#222222');
+  }
 }
