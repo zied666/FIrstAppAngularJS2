227992190fc4f63debc01cf45d9da9f14554daa7
Zied Kharraz <zied.kharraz@gmail.com>
2016-11-29 22:57:07 +0100

    improve and fix hotel list

--- app/shared/components/loading.component.ts
+++ app/shared/components/loading.component.ts
@@ -2,7 +2,11 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'loading',
-    template: '<div class="animated loading"><img style="display: block; margin-left: auto; margin-right: auto" src="assets/img/loading.jpg" /></div>'
+    template: `
+                <div class="animated loading">
+                    <img style="display: block; margin-left: auto; margin-right: auto" src="assets/img/loading.jpg" />
+                </div>
+`
 })
 export class LoadingComponent {
 }
